92d9a9d2e51cc54e3f57383ad6ccfb2451773146
Shanmukha Ramakrishna Vedantam <vrama91@hebb.ece.vt.edu>
2015-08-05 22:34:36 -0400

    updated Readme

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-examplepackage.torch
+torch-utilities
 ====================
 
-A hello-world for torch packages
+Some useful utilities for using torch with lua
 
-You can install the package by opening a terminal, changing directory into the folder and typing:
+##Installation
+1. Clone/download the repository
+2. Run "luarocks make"
 
-luarocks make
+##Usage
+Loading the library:
+"require 'vtutils'"
+
+##Contents
+1. obtain_gpu_lock_id.lua -- Utilities for checking if GPUs are free. 
+
--- obtain_gpu_lock_id.lua
+++ obtain_gpu_lock_id.lua
@@ -3,21 +3,24 @@
 --         File:  gpu_lock.lua
 --
 --        Usage: From any Torch or Lua Shell, use following commands
---        		require 'obtain_gpu_lock_id'
---        		**To get an id: **
---        		id = get_id() -- status.
---        		**To release an id: **
---        		release_id(id)
+--        		> require 'obtain_gpu_lock_id'
 --
+--        		To obtain and lock an id:
+--        		> id = obtain_gpu_lock_id.get_id() -- status.
+--        		The lock is automatically freed when the parent terminates
+--
+--				*NOT RECOMMENDED*
+--        		To get an id that won't be freed:
+--        		> id = obtain_gpu_lock_id.get_hog_id() -- status.
+--        		You *must* manually free these ids:
+--        		> obtain_gpu_lock_id.release_id(id)
+--				
 --  Description:  Wrapper for gpu_lock in Lua. 
 --              
---              *Not Implemented*
---              # TO-DO
 --  			If an ID lock is obtained in the default way, the lock is 
 --  			automatically freed when the parent process ends (even if it crashes) 
 --  			or on a system reboot. 
 --
---				*This is implemented*
 --  			It is also possible to get a lock that will persist 
 --  			and that must be manually freed. Make sure you release the ID you 
 --  			obtain using this code.
@@ -55,7 +58,7 @@ end ----- end of function error_check -----
 
 obtain_gpu_lock_id = {}
 
-function obtain_gpu_lock_id.get_id()                               -- get an ID to run GPU jobs on
+function obtain_gpu_lock_id.get_hog_id()                               -- get an ID to run GPU jobs on
 		-- If ID is -1, i.e. no GPU is available, torch will run jobs on CPU
 		error_check()
 		ID = tonumber(os.capture("exec " .. path_to_gpu_lock .. " --id-to-hog")) -- Run the gpu lock utility
@@ -63,7 +66,24 @@ function obtain_gpu_lock_id.get_id()                               -- get an ID 
 				print('No GPU available, Torch will run job on CPU with this ID')
 		end
 		return ID
-end  -----  end of function get_id  -----
+end  -----  end of function get_hog_id  -----
+
+------------------------------------------------------------------------
+--         Name:  obtain_gpu_lock_id.get_release_id
+--      Purpose:  Obtain a gpu lock ID and release it once the process quits
+--   Parameters:  None
+--      Returns:  GPU ID / -1 for CPU
+------------------------------------------------------------------------
+
+function obtain_gpu_lock_id.get_id()
+		error_check()
+		ID = tonumber(os.capture("exec " .. path_to_gpu_lock .. " --id")) -- Run the gpu lock utility
+		if(ID == -1) then
+				print('No GPU available, Torch will run job on CPU with this ID')
+		end
+		return ID
+end  -----  end of function obtain_gpu_lock_id.get_id()  -----
+
 
 
 function obtain_gpu_lock_id.release_id ( ID)                       -- release the ID GPU jobs were running on
